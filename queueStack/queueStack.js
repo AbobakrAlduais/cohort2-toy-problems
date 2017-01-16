dae1b8b8d7a06046fa21caba3f48e544b14a264f
AbobakrAlduais <aalduais@gmail.com>
2017-01-16 09:30:51 +0200

    tset

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -8,20 +8,26 @@
    */
  var Stack = function() {
   this.stack=[];
+  
 
    // add an item to the top of the stack
    this.push = function(value){
+    this.stack.push(value);
+    return this.stack;
    };
 
    // remove an item from the top of the stack
    this.pop = function(){
+    var val = this.stack.pop()
+    return val;
+
    };
 
    // return the number of items in the stack
    this.size = function(){
+    return this.stack.length;
    };
- };
-
+ }; 
  /**
    * Queue Class
    */
@@ -32,16 +38,22 @@
 
    // called to add an item to the `queue`
    this.enqueue = function(value){
+    inbox.push(value)
      
      }
-   };
+   
 
    // called to remove an item from the `queue`
    this.dequeue = function(){
+    for (var i = 0 ;i < inbox.length; i++){
+      outbox.push(inbox.pop())
     }
-   };
+    return outbox.pop();
+    }
+  
 
    // should return the number of items in the queue
    this.size = function(){
+    return outbox.length ;
    };
  };
