239ab38696c872a0ab75b6b2f5ef91cb09f59f73
AbobakrAlduais <aalduais@gmail.com>
2017-01-22 09:33:40 +0200

    t

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -22,5 +22,72 @@ ticTacToe(solvedBoard);//should return 1
 */
 
 function ticTacToe(board){
+	var hascolorrow = hasrowORColumSolved(board);
+	var hasdialog = hasDialogSolved(board);
+	var hasreversdialog = hasReversDialogSolved(board);
+	if(hascolorrow ===1 || hasdialog ===1 || hasreversdialog===1){
+		return 1
+	}
+	else if(hascolorrow ===2|| hasdialog ===2 || hasreversdialog===2){
+		return 2
+	}
+
+	else {return -1}
+
 	
+	 
 };
+
+
+function hasrowORColumSolved(board){
+	var countRow = 0;
+	var countCol = 0;
+	for(var j=0; j<board[0].length; j++){
+		countRow = 0;
+		countRow = 0;
+		for (var i = 0; i < board[0].length; i++){
+			countRow+=board[j][i];
+			countCol+=board[i][j]
+		}	
+			if(countRow === 3 || countCol === 3 ){
+				return 1
+			}
+			else if (countRow === 6 || countCol ===6){
+				return 2
+			}
+			
+	}	
+		
+}
+
+function hasDialogSolved(board){
+	count = 0;
+	for (var i = 0; i < board[0].length; i++){
+		count+=board[i][i];
+
+	}
+	if(count === 3){
+				return 1;
+			}
+			else if (count === 6){
+				return 2;
+			}
+}
+function hasReversDialogSolved(board){
+	count = 0;
+	var x = board[0].length;
+	for (var i = 0; i < board[0].length; i++){
+		x--;
+		count+=board[i][x];
+
+	}
+	if(count === 3){
+				return 1;
+			}
+			else if (count === 6){
+				return 2;
+			}
+}
+
+
+
